89e8e954a3d4146400a669d508db17c7d4eb2b4c
Hossein Fazel <fazel.hosein9654@gmail.com>
2026-02-17 15:26:47 +0330

    feat: Add config package

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module unitrip
 
-go 1.25.6
+go 1.24
+
+require github.com/joho/godotenv v1.5.1
---
+++ backend/internal/config/config.go
@@ -0,0 +1,9 @@
+package config
+
+type Config struct {
+	DatabaseName     string
+	DatabaseUser     string
+	DatabasePassword string
+	DatabasePort     int
+	DatabaseHost     string
+}
